3f09841c038c42b7f3f63fcb610026438317b292
conner <conner.canning@live.com>
2019-12-04 15:33:35 -0800

    more lat/lon changes

--- routes/weather.js
+++ routes/weather.js
@@ -23,7 +23,7 @@ router.get('/', (req, res) => {
 
 router.get("/weatherParams", (req, res) => { 
     const {lat, lon} = req.query;
-    const options = new URL('https://api.openweathermap.org/data/2.5/weather?lat={37.39}&lon={122.33}&APPID=256b3fac9d8ec8ce35e6be9487360e9c');
+    const options = new URL('https://api.openweathermap.org/data/2.5/weather?lat={47.606}&lon={-122.33}&APPID=256b3fac9d8ec8ce35e6be9487360e9c');
     //const options = new URL(`https://api.weatherbit.io/v2.0/current?lat=${lat}&lon=${lon}&key=f991b0a6c72941ecb4103f79eee8a9f2`);
     http.get(options, (resp) => {
         let responseString = '';
@@ -41,7 +41,7 @@ router.get("/weatherForecast", (req, res) => {
   const {lat, lon} = req.query;
   //const options = new URL('https://api.openweathermap.org/data/2.5/weather?lat={37.39}&lon={122.33}&APPID=256b3fac9d8ec8ce35e6be9487360e9c');
   // &days=10 only need 10 days of info :D don't over use webservice!
-  const options = new URL('https://api.weatherbit.io/v2.0/forecast/daily?lat=37.39&lon=-122.33&units=I&key=f991b0a6c72941ecb4103f79eee8a9f2');
+  const options = new URL('https://api.weatherbit.io/v2.0/forecast/daily?lat=47.606&lon=-122.33&units=I&key=f991b0a6c72941ecb4103f79eee8a9f2');
   //const options = new URL(`https://api.weatherbit.io/v2.0/current?lat=${lat}&lon=${lon}&key=f991b0a6c72941ecb4103f79eee8a9f2`);
   http.get(options, (resp) => {
       let responseString = '';
