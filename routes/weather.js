bc4dc68b50dfe799a3c2f679af5699d03a83b450
conner <conner.canning@live.com>
2019-12-04 15:24:29 -0800

    added imperial units params to api calls

--- routes/weather.js
+++ routes/weather.js
@@ -6,7 +6,7 @@ var router = express.Router();
 const http = require('https');
 
 router.get('/', (req, res) => { 
-    const options = new URL('https://api.openweathermap.org/data/2.5/weather?q=Seattle,us&APPID=256b3fac9d8ec8ce35e6be9487360e9c');
+    const options = new URL('https://api.openweathermap.org/data/2.5/weather?q=Seattle,us$units=imperial&APPID=256b3fac9d8ec8ce35e6be9487360e9c');
     //const options = new URL('https://api.weatherbit.io/v2.0/current?city=Seattle,WA&key=f991b0a6c72941ecb4103f79eee8a9f2')
     http.get(options, (resp) => {
         let responseString = '';
@@ -41,7 +41,7 @@ router.get("/weatherForecast", (req, res) => {
   const {lat, lon} = req.query;
   //const options = new URL('https://api.openweathermap.org/data/2.5/weather?lat={37.39}&lon={122.33}&APPID=256b3fac9d8ec8ce35e6be9487360e9c');
   // &days=10 only need 10 days of info :D don't over use webservice!
-  const options = new URL('https://api.weatherbit.io/v2.0/forecast/daily?lat=37.39&lon=122.33&key=f991b0a6c72941ecb4103f79eee8a9f2');
+  const options = new URL('https://api.weatherbit.io/v2.0/forecast/daily?lat=37.39&lon=122.33&units=I&key=f991b0a6c72941ecb4103f79eee8a9f2');
   //const options = new URL(`https://api.weatherbit.io/v2.0/current?lat=${lat}&lon=${lon}&key=f991b0a6c72941ecb4103f79eee8a9f2`);
   http.get(options, (resp) => {
       let responseString = '';
