bf9a5c8d6c44a8cbcdda8f1f77a771317de5cea7
conner <conner.canning@live.com>
2019-12-05 00:26:22 -0800

    lol fixed weatherParams?

--- routes/weather.js
+++ routes/weather.js
@@ -26,7 +26,7 @@ router.get('/', (req, res) => {
 
 router.get("/weatherParams", (req, res) => { 
     const {lat, lon} = req.query;
-    const options = new URL('https://api.openweathermap.org/data/2.5/weather?lat={47.6}&lon={-122.33}&APPID=256b3fac9d8ec8ce35e6be9487360e9c');
+    const options = new URL('https://api.openweathermap.org/data/2.5/weather?lat=47.606&lon=-122&APPID=256b3fac9d8ec8ce35e6be9487360e9c');
     //const options = new URL(`https://api.weatherbit.io/v2.0/current?lat=${lat}&lon=${lon}&key=f991b0a6c72941ecb4103f79eee8a9f2`);
     http.get(options, (resp) => {
         let responseString = '';
